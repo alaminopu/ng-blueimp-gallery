9179a3dd0fda1dd7f0c5a6425a0256ed88ff1ecf
Md. Al-Amin <alaminopu.me@gmail.com>
2017-01-31 14:01:04 +0600

    1. Added indicator options 2. Fix dynamic option capability 3. Added watcher to check if list is update by angular digest cycle

--- ng-blueimp-gallery.js
+++ ng-blueimp-gallery.js
@@ -22,12 +22,19 @@
         function link(scope, element, attr) {
             scope.slides = scope.list;
 
+            // Dynamically update list value.
+            scope.$watch('list', function(newVal, oldVal){
+                scope.slides = newVal;
+            });
+
             angular.element("#links").on('click', function(event){
                 event = event || $window.event;
                 var target = event.target || event.srcElement,
                     link = target.src ? target.parentNode : target,
                     options = {index: link, event: event},
                     links = this.getElementsByTagName('a');
+
+                angular.extend(options, scope.options);
 
                 if(blueimp){
                     blueimp.Gallery(links, options);
@@ -47,7 +54,7 @@
     uiGalleryTemplate.$inject = ['$templateCache'];
 
     function uiGalleryTemplate($templateCache){
-        $templateCache.put("gallery.html","<div id=\"blueimp-gallery\" class=\"blueimp-gallery blueimp-gallery-controls\">\n <div class=\"slides\"></div>\n <h3 class=\"title\"></h3>\n <a class=\"prev\">‹</a>\n <a class=\"next\">›</a>\n <a class=\"close\">×</a>\n <a class=\"play-pause\"></a>\n <ol class=\"indicator\"></ol>\n </div>\n\n <div id=\"links\" class=\"links blueimp-gallery-inner\">\n  <a ng-repeat=\"slide in slides\" href=\"{{slide.href}}\" title=\"{{slide.title}}\">\n    <img ng-src=\"{{slide.thumb}}\" alt=\"{{slide.title}}\">\n  </a>\n</div>\n");
+        $templateCache.put("gallery.html","<div id=\"blueimp-gallery\" class=\"blueimp-gallery blueimp-gallery-controls\">\n <div class=\"slides\"></div>\n <h3 class=\"title\"></h3>\n <a class=\"prev\">‹</a>\n <a class=\"next\">›</a>\n <a class=\"close\">×</a>\n <a class=\"play-pause\"></a>\n <ol class=\"indicator\">\n <li ng-repeat=\"slide in slides\" title=\"{{slide.title}}\" data-index=\"{{$index}}\" style=\"background-image: url({{slide.thumb}})\"></li>\n </ol>\n </div>\n\n <div id=\"links\" class=\"links blueimp-gallery-inner\">\n  <a ng-repeat=\"slide in slides\" href=\"{{slide.href}}\" title=\"{{slide.title}}\" data-index=\"{{$index}}\">\n    <img ng-src=\"{{slide.thumb}}\" alt=\"{{slide.title}}\">\n  </a>\n</div>\n");
     }
 
 })();
